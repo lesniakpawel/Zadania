62cda7bfe009d4d71fe1bd0238c2c6e7c6db126f
lesniak <lesniak.pawel.86@gmail.com>
2015-06-17 17:04:54 +0200

    second commit

--- 2-1.js
+++ 2-1.js
@@ -1,1 +1,20 @@
-some script
+//--------------------------------------------------
+//opcja pierwsza, z petla "do while"
+
+var hashTag = "#";
+do {
+	console.log(hashTag);
+	hashTag += "#";
+} while (hashTag.length<8);
+
+
+
+//--------------------------------------------------
+//z petla "for"
+
+var hashTag = "#";
+for (counter = 1; counter <= 7; counter++) {
+	console.log(hashTag);
+	hashTag += "#";
+}
+
