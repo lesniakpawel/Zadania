f09a63b48b3b08f032c6c5391396bbe8fc73ed28
lesniakpawel <lesniak.pawel.86@gmail.com>
2015-08-31 04:36:41 +0100

    chapter 5 - fix do 5-1

--- 5-1.js
+++ 5-1.js
@@ -1,7 +1,15 @@
-function flatten(array) {
-  return array.reduce(function(a, b) {
-    return a.concat(b);
-  })
+function flatten(some_array) {
+	var outcome = [];
+	for (var i = 0; i < some_array.length; i++) {
+		if (Array.isArray(some_array[i])) {
+			outcome = outcome.concat(flatten(some_array[i]));
+		} else {
+			outcome.push(some_array[i]);
+		}
+	}
+	return outcome;
 }
 
-console.log(flatten([[1, 2, 3], [4, 5], [6, 7]]));
+console.log(flatten([[[[[1]]]]]));
+console.log(flatten([[[2]],[1], [[[3,2]]]]));
+console.log(flatten([1, [2.1, 2.2, [2.31, 2.32, 2.33], 2.3], [3], 4, 5, [6.1, 6.2]]));
